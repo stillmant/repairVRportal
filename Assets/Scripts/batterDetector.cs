e55d2c130d27a806e7709b44ac517ee8695c74a1
terangg <twesa96@gmail.com>
2020-02-01 20:40:11 -0800

    created portal scene and added battery snapping

---
+++ Assets/Scripts/batterDetector.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class batterDetector : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+	private void OnTriggerEnter(Collider other)
+	{
+		if (other.tag == "battery") {
+			Destroy(other.GetComponent<OVRGrabbable>());
+			other.GetComponent<Renderer>().materials[0].color = Color.green;
+			other.transform.position = transform.position;
+			other.transform.rotation = transform.rotation;
+			other.transform.GetComponent<Rigidbody>().constraints = RigidbodyConstraints.FreezeAll;
+			//Debug.Log("ENTERED");
+		}
+	}
+}
