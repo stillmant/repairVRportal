0ada2a7bd1629ba375eaaf218d5aeb1ca1da7c6e
stillmant <stillmant98@gmail.com>
2020-02-01 18:16:28 -0800

    battery simple working

---
+++ Assets/Scripts/goalDetection.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class goalDetection : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "battery") {
+            other.GetComponent<OVRGrabbable>().enabled = false;
+            other.GetComponent<Renderer>().materials[0].color = Color.green;
+            Debug.Log("ENTERED");
+        }
+    }
+}
